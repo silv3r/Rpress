c1d2bc7a8f9a39ffb3445819c2e41a2f6fa1d1f5
unknown <Linus@.prevent.local>
2011-06-09 19:56:52 +0200

    Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rpress::Application.config.secret_token = 'dd7444be4210f02ab9ae4a1d78415a5e4a80700ce8339bc5f834ce99fec7c20f633e9bdb88e240cff80c0ebb64daeedcec26fa9bcbf666ccd30f08ff3c3734e5'
